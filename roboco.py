65b8da483ecc522d6d50d2f8b5ab270e2b4a20e4
Aaron Thapa <notsmart98@gmail.com>
2021-03-04 23:43:52 -0500

    ok i think this is it

--- roboco.py
+++ roboco.py
@@ -4,7 +4,7 @@ from typing import List
 
 client = discord.Client(intents=discord.Intents.all())
 pinRoles: List[int] = eval(Path('roles.txt').read_text())
-visibleChannels: List[int] = eval(Path('channels.txt').read_text())
+invisibleChannels: List[int] = eval(Path('channels.txt').read_text())
 
 @client.event
 async def on_ready():
@@ -12,40 +12,51 @@ async def on_ready():
 
 @client.event
 async def on_message(message: discord.Message):
-    global pinRoles, visibleChannels
+    global pinRoles, invisibleChannels
     if message.author != client.user:
-        messageContent = message.content
-        if messageContent.startswith('.rbc'):
-            messageContent = messageContent[5:]
-            print(messageContent)
-            if messageContent.startswith('query'):
-                await message.channel.send('Roles who can pin: ' + str([message.guild.get_role(x).name for x in pinRoles]))
-            elif messageContent.startswith('forcopy'):
-                await message.channel.send('ids: ' + ' '.join(str(x) for x in pinRoles))
-            elif messageContent.startswith('pingset'):
-                if isContributor(message.author):
-                    pinRoles = [''.join(y for y in x if y.isdigit()) for x in messageContent[9:].split(' ')]
-                    roleFile = open('roles.txt', 'w')
-                    roleFile.write(str(pinRoles))
-                    roleFile.close()
+        if message.channel.id not in invisibleChannels:
+            messageContent = message.content
+            if messageContent.startswith('.rbc'):
+                messageContent = messageContent[5:]
+                print(messageContent)
+                if messageContent.startswith('queryc'):
+                    await message.channel.send("Channels the bot can't see: " + str([message.guild.get_channel(x).name for x in invisibleChannels]))
+                elif messageContent.startswith('query'):
+                    await message.channel.send('Roles who can pin: ' + str([message.guild.get_role(x).name for x in pinRoles]))
+                elif messageContent.startswith('forcopy'):
+                    await message.channel.send('ids: ' + ' '.join(str(x) for x in pinRoles))
+                elif messageContent.startswith('pingset'):
+                    if await isContributor(message.author):
+                        pinRoles = [int(''.join(y for y in x if y.isdigit())) for x in messageContent[9:].split(' ')]
+                        roleFile = open('roles.txt', 'w')
+                        roleFile.write(str(pinRoles))
+                        roleFile.close()
+                    else:
+                        await message.channel.send('nice try')
+                elif messageContent.startswith('set'):
+                    if await isContributor(message.author):
+                        pinRoles = [int(x) for x in messageContent[5:].split(' ')]
+                        roleFile = open('roles.txt', 'w')
+                        roleFile.write(str(pinRoles))
+                        roleFile.close()
+                    else:
+                        await message.channel.send('nice try')
+                elif messageContent.startswith('channelm'):
+                    if await isContributor(message.author):
+                        invisibleChannels = [int(''.join(y for y in x if y.isdigit())) for x in messageContent[10:].split(' ')]
+                        channelFile = open('channels.txt', 'w')
+                        channelFile.write(str(invisibleChannels))
+                        channelFile.close()
+                elif messageContent.startswith('channel'):
+                    if await isContributor(message.author):
+                        invisibleChannels = [int(x) for x in messageContent[8:].split(' ')]
+                        channelFile = open('channels.txt', 'w')
+                        channelFile.write(str(invisibleChannels))
+                        channelFile.close()
+                    else:
+                        await message.channel.send('nice try')
                 else:
-                    await message.channel.send('nice try')
-            elif messageContent.startswith('set'):
-                if isContributor(message.author):
-                    pinRoles = [int(x) for x in messageContent[5:].split(' ')]
-                    roleFile = open('roles.txt', 'w')
-                    roleFile.write(str(pinRoles))
-                    roleFile.close()
-                else:
-                    await message.channel.send('nice try')
-            elif messageContent.startswith('channel'):
-                if isContributor(message.author):
-                    visibleChannels = [int(x) for x in messageContent[8:].split(' ')]
-                    channelFile = open('channels.txt', 'w')
-                    channelFile.write(str(visibleChannels))
-                    channelFile.close()
-            else:
-                await message.channel.send('syntax error')
+                    await message.channel.send('syntax error')
 
 @client.event
 async def on_reaction_add(reaction: discord.Reaction, user: discord.User):
