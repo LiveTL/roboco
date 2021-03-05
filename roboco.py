8bdd489a8897519dc45484d4d7303fd591a01bd6
Aaron Thapa <notsmart98@gmail.com>
2021-03-04 23:28:08 -0500

    add role commands

--- roboco.py
+++ roboco.py
@@ -4,6 +4,7 @@ from typing import List
 
 client = discord.Client(intents=discord.Intents.all())
 pinRoles: List[int] = eval(Path('roles.txt').read_text())
+visibleChannels: List[int] = eval(Path('channels.txt').read_text())
 
 @client.event
 async def on_ready():
@@ -11,23 +12,40 @@ async def on_ready():
 
 @client.event
 async def on_message(message: discord.Message):
-    global pinRoles
+    global pinRoles, visibleChannels
     if message.author != client.user:
         messageContent = message.content
         if messageContent.startswith('.rbc'):
-            messageContent = messageContent[3:]
+            messageContent = messageContent[5:]
+            print(messageContent)
             if messageContent.startswith('query'):
-                message.channel.send('Roles who can ping: ' + str(pinRoles))
-            elif messageContent.startwith('set'):
-                if any(x.name.lower() == 'contributor' for x in message.author.roles):
-                    pinRoles = [int(x) for x in messageContent[2:].split(',')]
+                await message.channel.send('Roles who can pin: ' + str([message.guild.get_role(x).name for x in pinRoles]))
+            elif messageContent.startswith('forcopy'):
+                await message.channel.send('ids: ' + ' '.join(str(x) for x in pinRoles))
+            elif messageContent.startswith('pingset'):
+                if isContributor(message.author):
+                    pinRoles = [''.join(y for y in x if y.isdigit()) for x in messageContent[9:].split(' ')]
                     roleFile = open('roles.txt', 'w')
                     roleFile.write(str(pinRoles))
                     roleFile.close()
                 else:
-                    message.channel.send('nice try')
+                    await message.channel.send('nice try')
+            elif messageContent.startswith('set'):
+                if isContributor(message.author):
+                    pinRoles = [int(x) for x in messageContent[5:].split(' ')]
+                    roleFile = open('roles.txt', 'w')
+                    roleFile.write(str(pinRoles))
+                    roleFile.close()
+                else:
+                    await message.channel.send('nice try')
+            elif messageContent.startswith('channel'):
+                if isContributor(message.author):
+                    visibleChannels = [int(x) for x in messageContent[8:].split(' ')]
+                    channelFile = open('channels.txt', 'w')
+                    channelFile.write(str(visibleChannels))
+                    channelFile.close()
             else:
-                message.channel.send('syntax error')
+                await message.channel.send('syntax error')
 
 @client.event
 async def on_reaction_add(reaction: discord.Reaction, user: discord.User):
@@ -43,6 +61,9 @@ async def on_reaction_add(reaction: discord.Reaction, user: discord.User):
             await client.get_channel(796900918901080085).send(embed=sendEmbed)
         else:
             await reaction.message.channel.send("You don't have the proper role to pin that message")
+
+async def isContributor(user: discord.Member):
+    return any(x.name.lower() == 'contributor' for x in user.roles)
 
 async def userHasPin(reaction: discord.Reaction):
     return any(y.id in pinRoles for x in await reaction.users().flatten() for y in x.roles)
